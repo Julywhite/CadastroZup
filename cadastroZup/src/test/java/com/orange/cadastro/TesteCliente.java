b7cd47758eca7723884bdaa64082adfb92a49911
EIRFU <juliana.caldas.ext@bayer.com>
2021-10-01 19:18:50 -0300

    Inclusão classes de testes unitários

--- cadastroZup/src/test/java/com/orange/cadastro/TesteCliente.java
+++ cadastroZup/src/test/java/com/orange/cadastro/TesteCliente.java
@@ -2,36 +2,51 @@ package com.orange.cadastro;
 
 import com.orange.cadastro.dto.ClienteDtoRequisicao;
 import com.orange.cadastro.model.Cliente;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 
+
 public class TesteCliente {
 
-    @Test
-    public void criarCliente() {
+    Cliente cliente;
 
-        String nome = "Ana Maria";
-        String email = "teste@outlook.com";
-        String cpf = "597.579.090-50";
+    @BeforeEach
+    public void instanciaClienteParaTestes() {
+
+        String nome = "Ana Maria", email = "teste@outlook.com", cpf = "597.579.090-50";
         LocalDate dataNasc = LocalDate.parse("1991-01-01");
 
-        Cliente cliente = new Cliente(nome, email, cpf, dataNasc);
+        cliente = new Cliente(nome, email, cpf, dataNasc);
+    }
 
-        Assertions.assertNotNull( cliente );
+
+    @Test
+    @DisplayName("Testar criação de cliente")
+    public void criarCliente() {
+
+        Assertions.assertThat( cliente ).isNotNull();
     }
 
     @Test
-    public void testaMetodoDtoConverteCliente() {
+    @DisplayName("Testar método converteCliente da classe ClienteDtoRequeisição")
+    public void metodoDtoConverteCliente() {
 
-        String nome = "Ana Maria";
-        String email = "teste@outlook.com";
-        String cpf = "597.579.090-50";
+        String nome = "Ana Maria", email = "teste@outlook.com", cpf = "597.579.090-50";
         LocalDate dataNasc = LocalDate.parse("1991-01-01");
 
         ClienteDtoRequisicao dto = new ClienteDtoRequisicao(nome, email, cpf, dataNasc);
 
-        Cliente cli = dto.converteCliente();
+        Assertions.assertThat( dto.converteCliente() ).hasSameClassAs( cliente );
+    }
 
+    @Test
+    @DisplayName("Testar ClienteService se está salvando corretamente")
+    public void salvaCliente() {
+
+
+        //Assertions.assertThat( cli.getCpf() ).isEqualTo( "597.579.090-50" );
     }
 }
---
+++ cadastroZup/src/test/java/com/orange/cadastro/TesteEndereco.java
@@ -0,0 +1,6 @@
+package com.orange.cadastro;
+
+public class TesteEndereco {
+
+
+}
